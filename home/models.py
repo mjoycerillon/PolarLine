73b7fb80275785bdef1626fac8541397bf919040
mjoycerillon <58972611+mjoycerillon@users.noreply.github.com>
2021-04-11 16:15:58 -0400

    Adding Product Table and display in admin page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    productName = models.CharField(max_length=100)
+    productPrice = models.FloatField(default=0)
+    productImage = models.ImageField(upload_to='shop/images/')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.productName
