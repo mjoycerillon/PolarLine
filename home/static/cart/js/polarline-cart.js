ba1bb1df5a72469c7f0e6e76d558cf46a1b2e389
mjoycerillon <58972611+mjoycerillon@users.noreply.github.com>
2021-04-13 19:39:33 -0400

    Removing unnecessary click events on cart page

--- home/static/cart/js/polarline-cart.js
+++ home/static/cart/js/polarline-cart.js
@@ -8,38 +8,7 @@ $(function() {
             $('#txtCartTotal').text("$"+cartSubTotal.toFixed(2))
         });
 
-        $('.cart-minus').on('click', function() {
-            var currentQuantity = $(this).parents('.cartItem').find('.cartItemQuantity').val();
-            if (currentQuantity > 0) {
-                $(this).parents('.cartItem').find('.cartItemQuantity').val(parseInt(currentQuantity) - 1);
-                updateItemSubtotal($(this));
-            }
-        });
-
-        $('.cart-add').on('click', function() {
-            var currentQuantity = $(this).parents('.cartItem').find('.cartItemQuantity').val();
-            $(this).parents('.cartItem').find('.cartItemQuantity').val(parseInt(currentQuantity) + 1);
-            updateItemSubtotal($(this));
-        });
-
-        function updateItemSubtotal(element){
-            var itemPrice = $(element).parents('.cartItem').find('.cartItemPrice').text().substr(1);
-            var itemQuantity = $(element).parents('.cartItem').find('.cartItemQuantity').val();
-            var subTotal = parseFloat(itemPrice) * parseFloat(itemQuantity);
-            var currentSubTotal = parseFloat($(element).parents('.cartItem').find('.cartSubTotal').text().substr(1));
-            var cartSubTotal = (parseFloat($('#txtCartSubTotal').text().substr(1)) - currentSubTotal) + subTotal;
-            $(element).parents('.cartItem').find('.cartSubTotal').text('$'+ subTotal.toFixed(2));
-            $('#txtCartSubTotal').text("$"+cartSubTotal.toFixed(2));
-            $('#txtCartTotal').text("$"+cartSubTotal.toFixed(2))
-        }
-
         $('.removeItem').on('click', function() {
-            var element = $(this).parents('.cartItem');
-            var price = $(this).parent().next().find('.cartSubTotal').text().substr(1);
-            var cartSubTotal = parseFloat($('#txtCartSubTotal').text().substr(1)) - parseFloat(price);
-            $('#txtCartSubTotal').text("$"+cartSubTotal.toFixed(2));
-            $('#txtCartTotal').text("$"+cartSubTotal.toFixed(2))
-
             element.animate({opacity: '0'}, 150, function(){
                 element.animate({height: '0px'}, 150, function(){
                     element.remove();
