e6f1639b661cd892b4d043103c6455067fc6c9ca
mjoycerillon <58972611+mjoycerillon@users.noreply.github.com>
2021-04-15 13:37:33 -0400

    Adding promotions table

--- home/models.py
+++ home/models.py
@@ -3,14 +3,10 @@ from django.db import models
 
 
 # Create your models here.
-from django.urls import reverse
-
-
 class Product(models.Model):
     productName = models.CharField(max_length=100)
     productPrice = models.FloatField(default=0)
     productImage = models.ImageField(upload_to='shop/images/')
-    slug = models.SlugField(max_length=200, db_index=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -28,4 +24,13 @@ class Cart(models.Model):
 
     def __str__(self):
         return f'{self.user} | {self.productId.__str__()} | {self.quantity}'
+
+
+class Promotion(models.Model):
+    promoDesc = models.CharField(max_length=25)
+    promoImage = models.ImageField(upload_to='promo/images/')
+
+    def __str__(self):
+        return self.promoDesc
+
 
